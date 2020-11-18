dd64fc47c4429a5aab5fa188dcbb52f15eb53653
daehyeonhong <fholng@outlook.com>
2020-11-19 00:07:33 +0900

    te

--- MyProject/MyProject/loop.c
+++ MyProject/MyProject/loop.c
@@ -71,9 +71,9 @@ int main(void) {
 
     for (int i = 0; i < input; i++)
     {
-        for (int j = 0; j < i + 1; j++)
+        for (int j = 0; j < input; j++)
         {
-            printf("*");
+            printf( ? "*" : " ");
         }
         printf("\n");
     }
