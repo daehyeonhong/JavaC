9785f0c5f9ef34305cbff7db4c55512fd5e7746f
hongdae <fholng@outlook.com>
2020-11-24 01:07:12 +0900

    Codility BinaryGap

---
+++ Review/src/Test.java
@@ -0,0 +1,21 @@
+public class Test {
+    public static int solution(int N) {
+        String binaryStr = Integer.toBinaryString(N);
+        char[] chrs = binaryStr.toCharArray();
+
+        int gap = 0;
+        int result = 0;
+
+        for (char c : chrs) {
+            if (c == '0') {
+                ++gap;
+            } else {
+                if (gap > result) {
+                    result = gap;
+                }
+                gap = 0;
+            }
+        }
+        return result;
+    }
+}
